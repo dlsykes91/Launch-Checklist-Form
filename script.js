364f8f06403aeb8b0ee61b1348650284e873a97c
Dominique <dlsykes91@gmail.com>
2020-06-01 01:27:40 -0400

    Getting Set up

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 // Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+     form.addEventListener("submit", function(event) {         
+       let pilotNameInput = document.querySelector("input[name=pilotName]");
+       let copilotNameInput = document.querySelector("input[name=copilotName]");
+       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+       let cargoMassInput = document.querySelector("input[name=cargoMass]");
+ 
+ // Check that all form boxes have value
+       if (pilotNameInput.value === "" || copilotNameInput.value === "") {
+       alert("All fields are required!");
+ 
+ // stop the form submission
+       event.preventDefault();
+       } else if (fuelLevelInput.value === "" || cargoMassInput.value === "") {
+       alert("All fields are required!");
+ 
+ // stop the form submission
+       event.preventDefault();
+       } 
+      
+   }); // close eventlistner submit
+ 
+ }); // close loadlistner
+    
+ 
+ /* This block of code shows how to format the HTML once you fetch some planetary JSON!
+ <h2>Mission Destination</h2>
+ <ol>
+    <li>Name: ${}</li>
+    <li>Diameter: ${}</li>
+    <li>Star: ${}</li>
+    <li>Distance from Earth: ${}</li>
+    <li>Number of Moons: ${}</li>
+ </ol>
+ <img src="${}">
+ */
+ 
